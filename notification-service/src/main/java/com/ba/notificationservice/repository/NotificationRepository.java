03e498f51951e57e6055fc65e5f6f6b9be32859a
flanux <forlinux40@gmail.com>
2025-12-06 07:21:20 +0000

    test done

---
+++ notification-service/src/main/java/com/ba/notificationservice/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package com.ba.notificationservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ba.notificationservice.model.Notification;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long>{
+
+    List<Notification> findByCustomerId(Long customerId); 
+}
